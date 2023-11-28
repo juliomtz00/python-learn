2b5f7693d2a2e84515441829079ec402cee49b2e
juliomtz00 <julio.mtz00@hotmail.com>
2023-11-27 21:45:37 -0600

    oop

--- oop/main.py
+++ oop/main.py
@@ -2,6 +2,10 @@ class Item:
     # The function init will be used whenever the class or object is called
     # Variable types can be added to the inputs to make them more robus
     # If a default value is set as in quantity, the type is automatically set.
+
+    pay_rate = 0.8
+    all = []
+
     def __init__(self, name: str,price: float,quantity=0):
         # assert statements are a used keyword that is used to check if there's a match with what is expected
         # Run validations to the recieved arguments
@@ -13,12 +17,25 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_prince(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
+
+    def instantiate_from_csv(self,):
+        
+    # Function to represent this item
+    def __repr__(self):
+        return f"Item('{self.name}','{self.price}','{self.quantity}')"
 
 item1 = Item("Phone",100,5)
 item2 = Item("Laptop",1000,3)
+item3 = Item("Cable",10,5)
+item4 = Item("Mouse",50,5)
+item5 = Item("Keyboard",75,5)
 
-print(item1.calculate_total_prince())
-print(item2.calculate_total_prince())
-
+print(Item.all)
